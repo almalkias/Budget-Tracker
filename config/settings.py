38ebbedae3e6860d425c806ef09654dba51eedb4
almalkias <almalkias@icloud.com>
2026-05-17 16:02:50 +0300

    Auto-create logs directory on startup to fix missing dir error

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SMS_WEBHOOK_SECRET = config('SMS_WEBHOOK_SECRET')
 GEMINI_API_KEY = config('GEMINI_API_KEY', default='')
 
+BASE_DIR.joinpath('logs').mkdir(exist_ok=True)
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
